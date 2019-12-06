4164cf12571c3d6713f0ffea8cfca54888a1fe78
nabaneetLahiri <42116611+nabaneetLahiri@users.noreply.github.com>
2019-12-07 02:55:55 +0530

    Add files via upload

--- entries/urls.py
+++ entries/urls.py
@@ -1,9 +1,11 @@
 
 
 from django.urls import path
-from .views import HomeView,EntryView,CreateEntryView
+from .views import HomeView,EntryView,CreateEntryView,UpdateEntryView,DeleteEntryView
 urlpatterns = [
 path('',HomeView.as_view(),name='blog-home'),
 path('entry/<int:pk>/',EntryView.as_view(),name="entry-detail"),
+path('entry/<int:pk>/update',UpdateEntryView.as_view(success_url='/'),name="entry-update"),
+path('entry/<int:pk>/delete',DeleteEntryView.as_view(success_url='/'),name="entry-delete"),
 path('create_entry/',CreateEntryView.as_view(success_url='/'),name='create_entry')
 ]
--- entries/views.py
+++ entries/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView,CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Entry
 # Create your views here.
 class HomeView(LoginRequiredMixin,ListView):
-    model=Entry
+    model=Entry#.objects.get(entry_author__exact=.request.user)
     template_name='entries/index.html'
     context_object_name="blog_entries"
-    ordering=['entry_date']
+    ordering=['-entry_date']
     paginate_by=3
+    def get_queryset(self):
+        return Entry.objects.filter(
+        entry_author__exact=self.request.user
+        )
 class EntryView(LoginRequiredMixin,DetailView):
     model=Entry
     template_name='entries/entry_detail.html'
@@ -19,3 +23,23 @@ class CreateEntryView(LoginRequiredMixin,CreateView):
     def form_valid(self, form):
         form.instance.entry_author=self.request.user
         return super().form_valid(form)
+class UpdateEntryView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model=Entry
+    template_name="entries/create_entry.html"
+    fields=["entry_title","entry_text"]
+    def form_valid(self, form):
+        form.instance.entry_author=self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        entry=self.get_object()
+        if self.request.user==entry.entry_author:
+            return True
+        return False
+class DeleteEntryView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model=Entry
+    template_name='entries/confirm_delete.html'
+    def test_func(self):
+        entry=self.get_object()
+        if self.request.user==entry.entry_author:
+            return True
+        return False
