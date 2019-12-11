23cd9ef985c2436cfd732bdc32066f41867d43ad
nabaneetLahiri <42116611+nabaneetLahiri@users.noreply.github.com>
2019-12-11 21:00:43 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -74,15 +74,21 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+# Postgres SQL config
+import psycopg2
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+"""
+# SQLite database config
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
