43ef566068c4661182e86960af694e829fb109ad
nabaneetLahiri <42116611+nabaneetLahiri@users.noreply.github.com>
2019-12-11 21:10:00 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -79,6 +79,7 @@ import psycopg2
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 import dj_database_url
+DATABASES={}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 """
 # SQLite database config
